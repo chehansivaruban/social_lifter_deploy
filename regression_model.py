f1c6cf1b5bf444662c343c91ef9ef2a30bfc996b
Chehan Sivaruban <63904379+chehansivaruban@users.noreply.github.com>
2023-05-01 19:09:03 +0530

    data leak made

--- regression_model.py
+++ regression_model.py
@@ -21,6 +21,7 @@ def get_dt_pred(date, time, isTagged, isLocation, isHashtag, isCashtag, follower
     sentiment_map = {'positive': 0, 'negative': 1, 'neutral': 2}
     sentiment_encoded = sentiment_map[sentiment]
     Followers_to_Following = followers / following
+    Followers_to_Following = Followers_to_Following.round(2)
     inputs = pd.DataFrame({
     "time": [time],  # add missing value
     "Day of week": [day_of_week_encoded],
@@ -37,7 +38,7 @@ def get_dt_pred(date, time, isTagged, isLocation, isHashtag, isCashtag, follower
     "polarity": [polarity],
     "sentiment": [sentiment_encoded],
     "topics": [topic],  # add missing value
-    "Followers_to_Following": [Followers_to_Following.round(2)],  # add missing value
+    "Followers_to_Following": [Followers_to_Following],  # add missing value
     })
 
     df = pd.DataFrame(inputs)
